4985678ace607d894301edd427594d3f0eed0c22
DenisPetrov <den794@mail.ru>
2019-05-16 19:37:36 +0300

    Test commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './hoc/ScrollToTop';
 import Layout from './hoc/Layout';
+import Header from './components/Layout/Header/Header';
+import Footer from './components/Layout/Footer/Footer';
 
 
 function App() {
@@ -9,10 +11,12 @@ function App() {
     <BrowserRouter>
       <ScrollToTop>
         <Layout>
+          <Header/>
           <Switch>
             <Route/>
           </Switch>
         </Layout>
+        <Footer/>
       </ScrollToTop>
     </BrowserRouter>
   );
